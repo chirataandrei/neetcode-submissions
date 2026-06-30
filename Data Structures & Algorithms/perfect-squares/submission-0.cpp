563e6136c2ad1736a6dca18599bd0e8662930b57
Chirata Andrei <andrei.chirata2006@gmail.com>
2026-06-30 13:45:20 +0300

    Add: perfect-squares - submission-0

---
+++ Data Structures & Algorithms/perfect-squares/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            dp[i] = 1 + dp[i - 1];
+            for (int j = 2; j * j <= i; j++) {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+};
